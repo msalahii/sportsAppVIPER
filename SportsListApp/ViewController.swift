9ca17ce619ad2f789c00afb3360d0e16690d046a
muhammadsalah <msalahii@yahoo.com>
2021-10-04 13:40:12 +0200

    Initial Commit

---
+++ SportsListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsListApp
+//
+//  Created by Muhammad Salah on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
